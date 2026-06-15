87078a9c00ae72fb9f87d819b98bb6c15539ae22
govo-web <aburrasel@gmail.com>
2026-06-15 10:23:06 +0000

    initial GOVO API builder

---
+++ src/server.js
@@ -0,0 +1,1 @@
+require("dotenv").config();const app=require("./app");const PORT=process.env.PORT||5000;app.listen(PORT,()=>console.log("GOVO API running "+PORT));
---
+++ src/store.js
@@ -0,0 +1,1 @@
+module.exports={merchants:[],riders:[],orders:[],areas:["Meherpur","Gangni","Mujibnagar","Bamundi","Amjhupi"],services:["Delivery","Parcel","Food","Medicine","Grocery"]};
